3d5be6830273cda793c2fa0a8b3228abd9b022f4
a.klimov <Klim-812@yandex.ru>
2022-02-12 19:12:53 +0300

    Исправление задачи 2

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,14 +1,21 @@
 package ru.netology
 
 fun main() {
-    val likes = 22
+    val likes = 1000
+    val chislo = likes % 10
+    val chislo2 = likes % 100
     val person = "человеку"
     val people = "людям"
+    val person2 = "человек"
 
 
     val text =
-        if (likes == 1 || likes == 21 || likes == 31) "Понравилось $likes $person" else "Понравилось $likes $people"
+        if (chislo == 1 && chislo2 !== 11)
+            "Понравилось $likes $person"
+        else if (likes == 1000) "Понравилось $likes $person2"
+        else "Понравилось $likes $people"
     println(text)
 
-
 }
+
+
